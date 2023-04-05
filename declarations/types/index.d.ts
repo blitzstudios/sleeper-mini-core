e9b8ffe3fc5ce9a21589dd94c1e29d100d28be30
Jason Lee <123986245+sleeper-jasonl@users.noreply.github.com>
2023-04-04 17:44:01 -0700

    Fix navigation declarations (#2)

--- declarations/types/index.d.ts
+++ declarations/types/index.d.ts
@@ -1,6 +1,6 @@
 import { NAVIGATION_ID, NAVIGATION_TYPE } from './redux/native_nav/constants.d';
-export declare type NavigationType = typeof NAVIGATION_TYPE;
-export declare type NavigationTypeId = typeof NAVIGATION_ID;
+export declare type NavigationType = typeof NAVIGATION_TYPE[keyof typeof NAVIGATION_TYPE];
+export declare type NavigationTypeId = typeof NAVIGATION_ID[keyof typeof NAVIGATION_ID];
 export * from './shared/graphql.d';
 export declare type Navigation = {
     selectedNavType: NavigationType;
