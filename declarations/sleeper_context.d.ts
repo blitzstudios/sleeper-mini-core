3a8063374917d11b6bc8fffa9f2a21592546d077
Jason Lee <jasonl@sleeper.app>
2023-05-04 12:23:38 -0700

    Update exposed APIs

--- declarations/sleeper_context.d.ts
+++ declarations/sleeper_context.d.ts
@@ -1,4 +1,4 @@
-import { User, Navigation, League, LeaguesMap, RostersInLeagueMap, UserMap, MatchupsInLeagueMap } from './types';
+import { User, Navigation, League, LeaguesMap, RostersInLeagueMap, UserMap, MatchupsInLeagueMap, UsersInLeagueMap, PlayoffsInLeagueMap, TransactionsInLeagueMap, TransactionsMap, SportInfoMap, DraftsInLeagueMap, DraftPickTradesInLeagueMap, DraftPicksInDraftMap, PlayersInSportMap } from './types';
 import SleeperActions from './sleeper_actions';
 declare class SleeperContext {
     static apiLevel: string;
@@ -10,6 +10,15 @@ declare class SleeperContext {
     rostersInLeagueMap: RostersInLeagueMap;
     userMap: UserMap;
     matchupsInLeagueMap: MatchupsInLeagueMap;
+    usersInLeagueMap: UsersInLeagueMap;
+    playoffsInLeagueMap: PlayoffsInLeagueMap;
+    transactionsInLeagueMap: TransactionsInLeagueMap;
+    transactionsMap: TransactionsMap;
+    sportInfoMap: SportInfoMap;
+    draftsInLeagueMap: DraftsInLeagueMap;
+    draftPickTradesInLeagueMap: DraftPickTradesInLeagueMap;
+    draftPicksInDraftMap: DraftPicksInDraftMap;
+    playersInSportMap: PlayersInSportMap;
     actions: SleeperActions;
     constructor();
 }
