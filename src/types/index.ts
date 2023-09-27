1be1f417d8559b45eb215e03e395988e794e2225
Luke Herbold <luke@sleeper.app>
2023-09-26 22:11:40 -0700

    Fixing type imports

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,7 @@
-import type SleeperEntitlement from '../../declarations/sleeper_entitlement.d';
-import type SleeperHeaderOptions from '../../declarations/sleeper_header_options.d';
+import type { HeaderOptions, Entitlement } from '../../declarations/types';
 
 export * from '../../declarations/types/index.d';
 export type { default as Context } from '../../declarations/sleeper_context.d';
-export type { default as Entitlements } from '../../declarations/sleeper_entitlement.d';
-export type { default as HeaderOptions } from '../../declarations/sleeper_header_options.d';
 export type { default as SocketMessage } from '../../declarations/sleeper_message.d';
 
 export type Config = {
@@ -14,6 +11,6 @@ export type Config = {
   remoteSocketPort?: number,
   dev?: boolean,
   logsEnabled?: boolean,
-  entitlements?: SleeperEntitlement[],
-  headerOptions?: SleeperHeaderOptions,
+  entitlements?: Entitlement[],
+  headerOptions?: HeaderOptions,
 };
