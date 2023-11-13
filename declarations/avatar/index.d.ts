dbd5f171a9020399a336e524f11a0b30691ccc3a
Jason Lee <jasonl@sleeper.app>
2023-11-13 13:48:29 -0800

    updated definitions

--- declarations/avatar/index.d.ts
+++ declarations/avatar/index.d.ts
@@ -1,23 +1,22 @@
-/// <reference types="react" />
 import { League, Player, User } from "../types";
 export interface AvatarProps {
     user: User;
     width?: number;
 }
-export declare const Avatar: (props: AvatarProps) => JSX.Element;
+export declare const Avatar: any;
 export interface AvatarPlayerProps {
     player: Player;
     width?: number;
 }
-export declare const AvatarPlayer: (props: AvatarPlayerProps) => JSX.Element;
+export declare const AvatarPlayer: any;
 export interface AvatarTeamProps {
     team: string;
     sport: string;
     width?: number;
 }
-export declare const AvatarTeam: (props: AvatarTeamProps) => JSX.Element;
+export declare const AvatarTeam: any;
 export interface AvatarLeagueProps {
     league: League;
     width?: number;
 }
-export declare const AvatarLeague: (props: AvatarLeagueProps) => JSX.Element;
+export declare const AvatarLeague: any;
